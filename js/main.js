66c7f26dd3eff4063250664862ed1b0e11ca2805
vinilzen <marchukilya@gmail.com>
2014-05-29 15:53:17 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+
+	$('#change-sity').popover({
+		placement: 'bottom',
+		html: true,
+		container:'.sm_w1_1popover',
+		trigger:'click',
+		content: function() {
+			return $('#change-sity-content').html();
+		}
+	});
+
+	$('#change-sity').on('shown.bs.popover', function () {
+
+		$('.sm_w1_1popover').css({
+			'width': $('.sm_w1_1popover').width(),
+			'height': $('.sm_w1_1popover').height(),
+			'top':0,
+			'left':0
+		});
+
+		$('.sm_w1_1close-change-sity').click(function() {
+			$('#change-sity').popover('hide');
+			return false;
+		});
+	});
+});
